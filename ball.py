7681ab15d73772b8eed6b37af403d238d2ef5a17
Jamie <jamielammas1@gmail.com>
2020-07-30 12:52:14 +0100

    Moved the Ball class to its own file

---
+++ ball.py
@@ -0,0 +1,43 @@
+import random
+
+class Ball:
+    def __init__(self, canvas, paddle, color):
+        self.canvas = canvas
+        self.paddle = paddle
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+        self.canvas.move(self.id, 245, 100)
+        starts = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(starts)
+        self.x = starts[0]
+        self.y = -3
+        self.canvas_height = self.canvas.winfo_height()
+        self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
+        self.count = 0
+        self.score_text = self.canvas.create_text(470, 20, text="Score: " + str(self.count))
+
+    def hit_paddle(self, pos):
+        
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+            return False
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, self.y)
+        pos = self.canvas.coords(self.id)
+        if pos[1] <= 0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if self.hit_paddle(pos) == True:
+            print(pos)
+            self.y = -2
+            self.count = self.count + 1
+            self.canvas.itemconfigure(self.score_text, text="Score: " + str(self.count))
+
+        if pos[0] <= 0:
+            self.x = 2
+        if pos[2] >= self.canvas_width:
+            self.x = -2
